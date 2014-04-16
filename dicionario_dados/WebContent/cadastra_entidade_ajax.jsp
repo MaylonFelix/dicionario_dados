cd4b89d47eb3c8f37e1bc0dd4344932ece1b8bc3
maylon.brito <maylon.brito@DEV09.pix.com.br>
2014-04-16 16:52:12 -0300

    ajax ok

--- dicionario_dados/WebContent/cadastra_entidade_ajax.jsp
+++ dicionario_dados/WebContent/cadastra_entidade_ajax.jsp
@@ -10,17 +10,19 @@
     		<title>Cadastra Entidade</title>
 
 			<script type="text/javascript"	src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.0/jquery.min.js"></script>
+			<script type="text/javascript"  charset="UTF-8" src="AjaxForm.js"></script>
 
 		</head>
 		<body>
 			<h1>Cadastra Entidade<h1 />
-			<form id="myAjaxRequestForm" name="cadastra_entidade" accept-charset="utf-8" action="CadastraEntidade">
+			<form id="myAjaxRequestForm" name="cadastra_entidade" accept-charset="utf-8">
 				Nome:<br /> 
 				<input id="nome" name="nome"	placeholder="Digite o nome da entidade" size="50" autofocus  /><br />
 				Descrição:<br />
 				<textarea rows="3" cols="40" id="descricao" name="descricao" placeholder="Digite a descrição da entidade" ></textarea><br /> 
 				<br /> 
 				<input type="button" value="Salvar" id="salvar" /> 
+				<!--<input type="button" value="Teste" id="teste" />-->
 			</form><br/>
 			<button onclick="window.location.href='ListaEntidades'">Lista Entidades</button>
 			<br /> 
--- dicionario_dados/src/br/com/pix/controller/CadastraEntidadeAjax.java
+++ dicionario_dados/src/br/com/pix/controller/CadastraEntidadeAjax.java
@@ -5,36 +5,39 @@ import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import br.com.pix.model.Country;
 import br.com.pix.model.Entidade;
 import br.com.pix.persistence.EntidadeDAO;
 
 /**
  * Servlet implementation class CadastraEntidade
  */
-@WebServlet("/CadastraEntidade")
-public class CadastraEntidade extends HttpServlet {
+@WebServlet("/CadastraEntidadeAjax")
+public class CadastraEntidadeAjax extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+	throws ServletException, IOException {
 		doPost(request, response);
 	}
-
+	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+	throws ServletException, IOException {
+		
 		// PRECISA SER DEFINIDO ASSIM ANTES DE ANDAR RSPOSTA PARA O NAVEGADOR
 		response.setContentType("application/json; charset=utf-8");
 		PrintWriter out = response.getWriter();
@@ -46,18 +49,24 @@ public class CadastraEntidade extends HttpServlet {
 
 		String nome = request.getParameter("nome");
 		String descricao = request.getParameter("descricao");
-		
+
+		Gson gson = new Gson();
+		JsonObject myObj = new JsonObject();//objeto json
+
 		EntidadeDAO entidadeDAO = new EntidadeDAO();
 		Entidade entidade = new Entidade();
 		entidade.setNome(nome);
 		entidade.setDescricao(descricao);
 		entidadeDAO.adiciona(entidade);
+			
+		JsonElement entidadeObj = gson.toJsonTree(entidade);
+		myObj.addProperty("success", true);
+		myObj.addProperty("msg", entidade.getNome()+" cadastrada com sucesso!");
+		myObj.add("entidade", entidadeObj);
 		
-		request.setAttribute("entidade", entidade);
-		
-		RequestDispatcher rd = request.getRequestDispatcher("resultado_cadastra_entidade.jsp");
-		
-		rd.forward(request, response);
+		out.println(myObj);
+		out.flush();
+		out.close();
 	}
 
 }
